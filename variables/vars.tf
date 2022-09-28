43ebd8ff314130fd61e36e49c1fa4f0e51458332
root <root@localhost.localdomain>
2022-09-28 00:10:59 -0700

    Variables Lab

--- variables/vars.tf
+++ variables/vars.tf
@@ -21,3 +21,35 @@ resource "null_resource" "operation1" {
     command = "echo ${var.myvar} having schedule of ${var.yourvar} days and is trainer Pavan : ${var.newvar} >> file1.txt"
     }
 }
+
+# List Var
+
+variable "users" {
+type = list
+default = ["user1","pavan","sam"]
+description = "list of users on the server"
+}
+
+resource "null_resource" "operation2" {
+    provisioner "local-exec" {
+    command = "echo users are :${var.users[0]}, ${var.users[1]} and ${var.users[2]} on server >> file1.txt"
+    }
+}
+
+# Map Var
+
+variable "flavors" {
+    type = map
+    default = {
+        "flavor1" = "1XCPU-1GB"
+        "flavor2" = "2XCPU-4GB"
+        "flavor9" = "8XCPU-64GB"
+    }
+    description = "Flavors we want for servers to users"
+}
+
+resource "null_resource" "operation3" {
+    provisioner "local-exec" {
+    command = "echo Configurations available for servers : ${var.flavors["flavor1"]} ,  ${var.flavors["flavor2"]},  ${var.flavors["flavor9"]} >> file1.txt"
+    }
+}
